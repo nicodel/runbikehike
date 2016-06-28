c981a39c37940007119af1afc163d1ca6ba4f1be
nicodel <nicolas@nicodel.com>
2016-06-28 16:56:01 +0200

    working on displaying Reports

--- sources/js/views/reports.js
+++ sources/js/views/reports.js
@@ -1,23 +1,24 @@
 /* jshint browser: true */
-/* globals Backbone, d3, crossfilter, dc, Preferences, Dashboard, utils */
+/* globals Backbone, d3, crossfilter, dc, Preferences, Dashboard, utils, Bodies */
 /* exported ReportsView */
 'use strict';
 
 var ReportsView = Backbone.NativeView.extend({
   el: '#reports-view',
 
-  // template: Template(document.getElementById('reports-template').innerHTML),
   weightChart : dc.lineChart('#reports-weight-graph'),
-  caloriesChart : dc.barChart('#reports-calories-graph'),
-  distanceChart : dc.barChart('#reports-distance-graph'),
-  durationChart : dc.barChart('#reports-duration-graph'),
+  // caloriesChart : dc.barChart('#reports-calories-graph'),
+  // distanceChart : dc.barChart('#reports-distance-graph'),
+  // durationChart : dc.barChart('#reports-duration-graph'),
 
 
   initialize: function() {
     this.collection = Dashboard;
 
-    this.listenTo(this.collection, 'sync', this.render);
-    this.listenTo(this.collection, 'reset', this.render);
+    // this.listenTo(this.collection, 'sync', this.render);
+    // this.listenTo(this.collection, 'reset', this.render);
+
+    this.listenTo(Bodies, 'sync', this.render);
 
     var that = this;
     document.getElementById('reports-select-date').addEventListener('change', function(el) {
@@ -51,9 +52,9 @@ var ReportsView = Backbone.NativeView.extend({
       }
       console.log('first - last', first, last);
       that.weightChart.focus([first, last]);
-      that.caloriesChart.focus([first, last]);
-      that.distanceChart.focus([first, last]);
-      that.durationChart.focus([first, last]);
+      //that.caloriesChart.focus([first, last]);
+      //that.distanceChart.focus([first, last]);
+      //that.durationChart.focus([first, last]);
     });
   },
 
@@ -69,42 +70,56 @@ var ReportsView = Backbone.NativeView.extend({
     var act_data = [];
     var weight_data = [];
     var item;
-    this.collection.forEach(function(model) {
+    Bodies.forEach(function(model) {
       item = model.attributes;
       item.formateddate = dateFormat.parse(item.date);
       item.month = d3.time.month(item.formateddate);
-      if (item.activity === 'weight_act') {
+      weight_data.push(item);
+    });
+
+    /*console.log('this.collection', this.collection);
+    this.collection.forEach(function(model) {
+      item = model.attributes;
+      console.log('item', item);
+      item.formateddate = dateFormat.parse(item.date);
+      item.month = d3.time.month(item.formateddate);
+      if (item.activity === 'weight') {
         weight_data.push(item);
       } else {
         act_data.push(item);
       }
-    });
-
+    });*/
+    console.log('weight_data', weight_data);
     var ndx_weight      = crossfilter(weight_data);
     var date_weight_dim = ndx_weight.dimension(function(d) {
+      console.log('d.month', d.month);
       return d.month;
     });
+
     var weightGroup     = date_weight_dim.group().reduceSum(function(d) {
+      console.log('d.value', d.value);
       return parseFloat(d.value, 10);
     });
-    this.weightChart/*.width(768).height(240)*/
-      .x(d3.time.scale().domain([new Date(new Date().getFullYear(), 0, 1), new Date(new Date().getFullYear(), 11, 31)]))
-      .dimension(date_weight_dim)
-      .renderHorizontalGridLines(true)
-      .renderVerticalGridLines(true)
-      .brushOn(false)
-      .mouseZoomable(false)
-      .yAxisLabel('Weight (kg)')
-      .group(weightGroup, 'Weight');
 
-    var ndx_act       = crossfilter(act_data);
+      this.weightChart/*.width(768).height(240)*/
+        .x(d3.time.scale().domain([new Date(new Date().getFullYear(), 0, 1), new Date(new Date().getFullYear(), 11, 31)]))
+        .dimension(date_weight_dim)
+        .renderHorizontalGridLines(true)
+        .renderVerticalGridLines(true)
+        .brushOn(false)
+        .mouseZoomable(false)
+        .yAxisLabel('Weight (kg)')
+        .colors(red)
+        .group(weightGroup, 'Weight');
+
+    /*var ndx_act       = crossfilter(act_data);
     var date_act_dim  = ndx_act.dimension(function(d) {
       return d.month;
     });
     var caloriesGroup = date_act_dim.group().reduceSum(function(d) {
       return parseInt(d.calories, 10);}
     );
-    this.caloriesChart/*.width(768).height(240)*/
+    this.caloriesChart
       .x(d3.time.scale().domain([new Date(new Date().getFullYear(), 0, 1), new Date(new Date().getFullYear(), 11, 31)]))
       .round(d3.time.month.round)
       .xUnits(d3.time.days)
@@ -125,7 +140,7 @@ var ReportsView = Backbone.NativeView.extend({
         false
       ).value;
     });
-    this.distanceChart/*.width(768).height(240)*/
+    this.distanceChart
       .x(d3.time.scale().domain([new Date(new Date().getFullYear(), 0, 1), new Date(new Date().getFullYear(), 11, 31)]))
       .round(d3.time.month.round)
       .xUnits(d3.time.days)
@@ -141,7 +156,7 @@ var ReportsView = Backbone.NativeView.extend({
     var durationGroup = date_act_dim.group().reduceSum(function(d) {
       return parseInt(d.duration, 10);
     });
-    this.durationChart/*.width(768).height(240)*/
+    this.durationChart
       .x(d3.time.scale().domain([new Date(new Date().getFullYear(), 0, 1), new Date(new Date().getFullYear(), 11, 31)]))
       .round(d3.time.month.round)
       .xUnits(d3.time.days)
@@ -152,7 +167,7 @@ var ReportsView = Backbone.NativeView.extend({
       .yAxisLabel('Duration (min)')
       .colors(red)
       .dimension(date_act_dim)
-      .group(durationGroup);
+      .group(durationGroup);*/
 
 
 
