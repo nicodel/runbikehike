53881104b8adec951e4f0062800ac42a29ddbf54
nicodel <nicolas@nicodel.com>
2016-07-07 16:59:41 +0200

    modyfing structure for Font Awesome to work on CC

--- scripts/publish-cozy.sh
+++ scripts/publish-cozy.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
 echo "Publishing to Cozy repository"
-rsync -a --delete ./public/ ./publish/cozy/public/
-rsync -a --delete ./server/ ./publish/cozy/server/
-rsync -a --delete ./server.js ./publish/cozy/server.js
+rsync -a --delete ./public/ ./publish/runbikehike-cozy/public/
+rsync -a --delete ./server/ ./publish/runbikehike-cozy/server/
+rsync -a --delete ./server.js ./publish/runbikehike-cozy/server.js
