c52019cdf18045efd5ccaea91cb1df61f5d49a80
Nicolas Delebecque <nicolas@nicodel.com>
2017-04-12 23:14:00 +0200

    modyfing fottball template file

--- sources/js/factories/activities/football.js
+++ sources/js/factories/activities/football.js
@@ -1,11 +1,14 @@
 'use strict';
-var activities  = activities || {};
-activities.list = activities.list || [];
-var views       = views || {};
-activities.football = {
+var RBH                     = RBH || {};
+RBH.Factory                 = RBH.Factory || {};
+RBH.Factory.Activities      = RBH.Factory.Activities || {};
+RBH.Factory.Activities.list = RBH.Factory.Activities.list || [];
+var views                   = views || {};
+
+RBH.Factory.Activities.football = {
   new_view                : [],
   summary_view_dashboard  : views.dashboard_summary_3,
   summary_view_sessions   : views.sessions_summary_3,
   detailled_view          : views.detailled_3
 };
-activities.list.push('football');
+RBH.Factory.Activities.list.push('football');
