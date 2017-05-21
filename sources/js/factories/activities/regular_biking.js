bb9fc9a4ac67339361a39ddc93f6b5192f19fb16
Nicolas Delebecque <nicolas@nicodel.com>
2017-05-21 22:45:28 +0200

    #34 enabling regular biking

--- sources/js/factories/activities/regular_biking.js
+++ sources/js/factories/activities/regular_biking.js
@@ -1,11 +1,14 @@
 'use strict';
-var activities  = activities || {};
-activities.list = activities.list || [];
-var views       = views || {};
-activities.regular_biking = {
+var RBH                     = RBH || {};
+RBH.Factory                 = RBH.Factory || {};
+RBH.Factory.Activities      = RBH.Factory.Activities || {};
+RBH.Factory.Activities.list = RBH.Factory.Activities.list || [];
+var views                   = views || {};
+
+RBH.Factory.Activities.regular_biking = {
   new_view                : ['import_form', 'distance', 'altitude'],
   summary_view_dashboard  : views.dashboard_summary_1,
   summary_view_sessions   : views.sessions_summary_1,
   detailled_view          : views.detailled_1
 };
-activities.list.push('regular_biking');
+RBH.Factory.Activities.list.push('regular_biking');
