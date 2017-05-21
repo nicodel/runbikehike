81dec3135e830722624986b3aae60f03c91c039c
Nicolas Delebecque <nicolas@nicodel.com>
2017-05-21 22:21:41 +0200

    #34 enabling handball and in_sea

--- sources/js/factories/activities/handball.js
+++ sources/js/factories/activities/handball.js
@@ -1,11 +1,14 @@
 'use strict';
-var activities  = activities || {};
-activities.list = activities.list || [];
-var views       = views || {};
-activities.handball = {
+var RBH                     = RBH || {};
+RBH.Factory                 = RBH.Factory || {};
+RBH.Factory.Activities      = RBH.Factory.Activities || {};
+RBH.Factory.Activities.list = RBH.Factory.Activities.list || [];
+var views                   = views || {};
+
+RBH.Factory.Activities.handball = {
   new_view                : [],
   summary_view_dashboard  : views.dashboard_summary_3,
   summary_view_sessions   : views.sessions_summary_3,
   detailled_view          : views.detailled_3
 };
-activities.list.push('handball');
+RBH.Factory.Activities.list.push('handball');
