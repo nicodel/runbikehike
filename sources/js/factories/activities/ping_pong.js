ce9c312a47f9d8acdb1431406e82c5c121f03d55
Nicolas Delebecque <nicolas@nicodel.com>
2017-05-21 22:41:12 +0200

    #34 enabling pushing

--- sources/js/factories/activities/ping_pong.js
+++ sources/js/factories/activities/ping_pong.js
@@ -1,11 +1,14 @@
 'use strict';
-var activities  = activities || {};
-activities.list = activities.list || [];
-var views       = views || {};
-activities.ping_pong = {
+var RBH                     = RBH || {};
+RBH.Factory                 = RBH.Factory || {};
+RBH.Factory.Activities      = RBH.Factory.Activities || {};
+RBH.Factory.Activities.list = RBH.Factory.Activities.list || [];
+var views                   = views || {};
+
+RBH.Factory.Activities.ping_pong = {
   new_view                : [],
   summary_view_dashboard  : views.dashboard_summary_3,
   summary_view_sessions   : views.sessions_summary_3,
   detailled_view          : views.detailled_3
 };
-activities.list.push('ping_pong');
+RBH.Factory.Activities.list.push('ping_pong');
